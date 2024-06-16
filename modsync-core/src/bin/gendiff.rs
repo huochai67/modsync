27811070fada00d8f62745facc7a1e498a495900
NiceFish4520 <kcass774@gmail.com>
2024-06-16 18:52:13 +0800

    remove unuse target

--- modsync-core/src/bin/gendiff.rs
+++ modsync-core/src/bin/gendiff.rs
@@ -7,15 +7,18 @@ async fn main() {
         .expect("get remote config error");
     let mut client = MSClient::config(&config);
     let client2: &MSClient = client.path("./".into());
+
+    if let Ok(locallist) = client2.get_modlist_local() {
+        println!("{:?}", locallist);
+    }
+
     match client2.get_modlist().await {
-        Ok(modlist) => {
-            match client2.get_difflist(modlist){
-                Ok(difflist) => {
-                    println!("{:?}", difflist);
-                },
-                Err(err) => panic!("{}", err),
+        Ok(modlist) => match client2.get_difflist(modlist) {
+            Ok(difflist) => {
+                println!("{:?}", difflist);
             }
-        }
+            Err(err) => panic!("{}", err),
+        },
         Err(err) => panic!("{}", err),
     }
 
