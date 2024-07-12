f6c667ba6e87e310c73ac6a74c22e102433c7bd7
NiceFish4520 <kcass774@gmail.com>
2024-07-13 03:31:53 +0800

    add title to titlebar

--- modsync-app/src/titlebar.tsx
+++ modsync-app/src/titlebar.tsx
@@ -1,10 +1,15 @@
 import { MinusIcon, MoonIcon, SunIcon, XMarkIcon } from "@heroicons/react/24/solid";
 import { Button, ButtonGroup } from "@nextui-org/react";
 import { getCurrent } from "@tauri-apps/api/window";
+import React from "react";
 
 export function TitleBar({ dark, onDarkChanged }: { dark: boolean, onDarkChanged: (() => void) }) {
+    const [title, settitle] = React.useState("Title")
+    getCurrent().title().then(settitle)
     return (
-        <div data-tauri-drag-region className="flex flex-row-reverse items-center h-full">
+        <div data-tauri-drag-region className="flex items-center h-full border-x-4 border-background">
+            <p>{title}</p>
+            <div className="grow"/>
             <ButtonGroup variant="light" size="sm">
                 <Button isIconOnly aria-label="tb-darkmode" onClick={onDarkChanged}>{dark ? <SunIcon /> : <MoonIcon />}</Button>
                 <Button isIconOnly aria-label="tb-minus" onClick={() => { getCurrent().minimize() }}><MinusIcon /></Button>
